e9e1d396d71a000e7d249b38cde62ccfc4d357d5
AlonsoSantos <ojimenez34@misena.edu.co>
2023-11-04 21:18:59 -0500

    sin datos personales

--- ProyectoWeb/ProyectoWeb/settings.py
+++ ProyectoWeb/ProyectoWeb/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp-mail.outlook.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "tilijobne@outlook.com"
-EMAIL_HOST_PASSWORD = "Shiawase23$12#&"
+EMAIL_HOST_USER = "correo outlook"
+EMAIL_HOST_PASSWORD = "contraseña"
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
